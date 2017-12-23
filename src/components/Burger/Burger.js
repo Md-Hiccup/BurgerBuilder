091e8a9364707f5cce3a52336aa46699e2fdf6ae
Md-Hiccup <ask2md@gmail.com>
2017-12-23 23:27:28 +0530

    adding burger and controls

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            console.log(igKey);
+            return [...Array(props.ingredients[igKey])].map((_ , i) => {
+                console.log("_ ",i);
+                return <BurgerIngredient key={igKey + i} type ={igKey} />;  // [ , ]
+            });
+        })
+        .reduce((arr, el ) =>{
+            console.log(arr+" "+el);
+            return arr.concat(el)
+        }, []);
+    if(transformedIngredients <= 0 ){
+        transformedIngredients = <p> Please start adding ingredients </p>
+    }
+    console.log(transformedIngredients);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type = "bread-top"/>
+            {transformedIngredients}
+            {/*<BurgerIngredient type = "cheese"/>*/}
+            {/*<BurgerIngredient type = "meat"/>*/}
+            <BurgerIngredient type = "bread-bottom"/>
+        </div>
+    );
+};
+
+export default burger ;
